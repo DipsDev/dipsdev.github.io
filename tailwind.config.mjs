83e7a8aa019ec65f5da0a542137b52dcb982c33d
Ido Geva <ido.geva.b@gmail.com>
2025-01-04 18:42:27 +0200

    add dark bright, split projects, add more projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'brighter': '#795757',
-				'bright': '#664343',
-				'dark': '#3D3D3D',
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brighter: "#795757",
+        bright: "#664343",
+        dark: "#3D3D3D",
+        "dark-bright": "#525252",
+      },
+    },
+  },
+  plugins: [],
+};
